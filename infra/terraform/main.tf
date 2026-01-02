f3cec90cc421eada5265e9b251ee14a2952bf105
shailesh jadav <shailesh.jadav@zuru.com>
2026-01-02 17:44:52 +0530

    feat(infra): Add ECS task definition for video transcoding

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -112,6 +112,49 @@ resource "aws_s3_bucket_notification" "raw_bucket_notification" {
   depends_on = [aws_sqs_queue_policy.video_upload_events_policy]
 }
 
+# ECS Task Definition for Video Transcoding
+resource "aws_ecs_task_definition" "video_transcoder_task" {
+  family                   = "video-transcoder-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "1024"
+  memory                   = "2048"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  # runtime_platform {
+  #   operating_system_family = "LINUX"
+  #   cpu_architecture        = "X86_64"
+  # }
+  
+  container_definitions = jsonencode([
+    {
+      name      = "transcoder"
+      image     = "${aws_ecr_repository.video_transcoder_repository.repository_url}:latest"
+      essential = true
+      
+      environment = [
+        {
+          name  = "INPUT_BUCKET"
+          value = aws_s3_bucket.raw_bucket.id
+        },
+        {
+          name  = "OUTPUT_BUCKET"
+          value = aws_s3_bucket.processed_bucket.id
+        },
+      ]
+      
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.ecs_log_group.name
+          "awslogs-region"        = var.region
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    }
+  ])
+}
+
 # Outputs
 output "raw_bucket_name" {
   description = "Name of the raw videos bucket"
@@ -151,5 +194,10 @@ output "sqs_queue_url" {
 output "sqs_queue_arn" {
   description = "ARN of the SQS queue"
   value       = aws_sqs_queue.video_upload_events.arn
+}
+
+output "ecs_task_definition_arn" {
+  description = "ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.video_transcoder_task.arn
 }
 
