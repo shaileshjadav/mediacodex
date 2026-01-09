1fcf58577b60e9ea42073b0dd57dcf19e2ef96b7
shaileshjadav.dev@gmail.com <zuru@ZTA-ShaileshJ.localdomain>
2026-01-09 11:02:19 +0530

    feat(backend): Remove Users table

--- backend/db/migrations/20251215045727_init.sql
+++ backend/db/migrations/20251215045727_init.sql
@@ -1,25 +1,17 @@
 -- migrate:up
-CREATE TABLE "users" (
-    "id" BIGSERIAL PRIMARY KEY,
-    "clerkUserId" VARCHAR(255) NOT NULL UNIQUE,
-    "name" VARCHAR(150) NOT NULL,
-    "email" VARCHAR(255) UNIQUE NOT NULL,
-    "password" TEXT,
-    "createdAt" TIMESTAMP DEFAULT NOW(),
-    "updatedAt" TIMESTAMP DEFAULT NOW()
-);
-
 CREATE TABLE "videos" (
     "id" BIGSERIAL PRIMARY KEY,
     "videoId" TEXT NOT NULL, -- UUID 
-    "userId" VARCHAR(255) NOT NULL REFERENCES users("clerkUserId"),
+    "userId" VARCHAR(255) NOT NULL, -- Clerk UserId
     "status" VARCHAR(20) NOT NULL 
         CHECK (status IN ('UPLOADED', 'PROCESSING', 'COMPLETED', 'FAILED')),
     "createdAt" TIMESTAMP DEFAULT NOW(),
     "updatedAt" TIMESTAMP DEFAULT NOW()
 );
+-- Unique video Id
+CREATE UNIQUE INDEX idx_videos_video_id ON "videos" ("videoId");
+-- UserId indexing
+CREATE INDEX idx_videos_user_id ON "videos" ("userId");
 
 -- migrate:down
-
 DROP TABLE IF EXISTS "videos";
-DROP TABLE IF EXISTS "users";
